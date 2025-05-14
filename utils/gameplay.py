3ebffc8b54e41544d8b2cd920f5affb2b650341a
ItzChaBoiGhoza <cguy925@gmail.com>
2025-05-14 14:39:22 -0600

    Coded how the gameplay should be inside of gameplay.py

--- utils/gameplay.py
+++ utils/gameplay.py
@@ -1,14 +1,18 @@
-def player_busts():
-    pass
+def player_busts(player, dealer, chips):
+    print("Player Busts!")
+    chips.lose_bet()
 
-def player_wins():
-    pass
+def player_wins(player, dealer, chips):
+    print("Player Wins!")
+    chips.win_bet()
 
-def dealer_busts():
-    pass
+def dealer_busts(player, dealer, chips):
+    print("Dealer Busts!")
+    chips.win_bet()
 
-def dealer_wins():
-    pass
+def dealer_wins(player, dealer, chips):
+    print("Dealer Wins!")
+    chips.lose_bet()
 
-def push():
-    pass
+def push(player, dealer):
+    print("Player and Dealer tie! It is a PUSH!")
